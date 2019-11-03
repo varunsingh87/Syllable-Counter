e9024528126ca028216f3be2010fa1329082de00
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-11-03 17:44:39 -0500

    Add support for if the word starts with a vowel

--- syllable-counter.py
+++ syllable-counter.py
@@ -2,4 +2,7 @@ import re
 word = input("Enter a word to find the syllable count: ")
 regex = r"([^aeiouy])(a|e|i|o|u|y)"
 matches = re.findall(regex, word, re.MULTILINE)
-print(len(matches))
+count = len(matches)
+if ['a', 'e', 'i', 'o', 'u'].__contains__(word[0]):
+    count+=1
+print(count)
