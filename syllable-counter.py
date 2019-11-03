31f0fdcfbb60c8c3541bc192b83cf875e964824d
KnowledgeableKangaroo <31084854+KnowledgeableKangaroo@users.noreply.github.com>
2019-11-03 17:09:49 -0500

    Write program

--- syllable-counter.py
+++ syllable-counter.py
@@ -1,1 +1,5 @@
-word = input("Enter a word to find the syllable count: \n");
+import re
+word = input("Enter a word to find the syllable count: ")
+regex = r"([^aeiouy])(a|e|i|o|u|y)"
+matches = re.findall(regex, word, re.MULTILINE)
+print(len(matches))
